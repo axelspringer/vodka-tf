83f1d50ac44240fb567b91c607c05bba2c262685
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-03-24 13:54:15 +0100

    ~ wip

--- ecs_traefik/ssl.tf
+++ ecs_traefik/ssl.tf
@@ -1,5 +1,5 @@
 # + get data AWS Certificate
 data "aws_acm_certificate" "default" {
   count  = "${length(var.branches)}"
-  domain = "*.${element(var.branches, count.index)}.${var.route53_wildcard_zone}"
+  domain = "*.${var.route53_wildcard_zone}"
 }
