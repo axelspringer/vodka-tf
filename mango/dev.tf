05aace72f282539b760a8f6aa350e701a026f256
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-05-18 19:34:15 +0200

    ~ wip

--- mango/dev.tf
+++ mango/dev.tf
@@ -13,36 +13,33 @@ data "aws_iam_policy_document" "read_trust_policy" {
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
       ]
     }
-  }
-}
 
-data "aws_iam_policy_document" "read_assume_policy" {
-  statement {
-    effect = "Allow"
+    condition {
+      test     = "Bool"
+      variable = "s3:aws:MultiFactorAuthPresent"
 
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    principals {
-      type = "AWS"
-
-      identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.cluster_name}-mango-read",
+      values = [
+        "true",
       ]
     }
   }
 }
 
-resource "aws_iam_role" "read" {
-  name        = "${var.cluster_name}-mango-read"
-  description = "This role allows read access to all relevant resources"
-  path        = "/ecs/"
+data "aws_iam_policy_document" "read_assume_policy" {
+  statement {
+    actions   = ["sts:AssumeRole"]
+    resources = ["${aws_iam_role.read.arn}"]
+  }
+}
 
+resource "aws_iam_role" "read" {
+  name               = "${var.cluster_name}-mango-read"
+  description        = "This role allows read access to all relevant resources"
+  path               = "/ecs/"
   assume_role_policy = "${data.aws_iam_policy_document.read_trust_policy.json}"
 }
 
-resource "aws_iam_role_policy" "read_assume_policy" {
+resource "aws_iam_policy" "read_assume_policy" {
   name        = "${var.cluster_name}-mango-read"
   description = "This policy allows to assume the ${var.cluster_name}-mango-read role"
   policy      = "${data.aws_iam_policy_document.read_assume_policy.json}"
