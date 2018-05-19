b78c4f078e48d47b23c4fe58f613872072dd3198
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-05-19 11:28:04 +0200

    ~ wip

--- mango/user.tf
+++ mango/user.tf
@@ -35,7 +35,6 @@ data "aws_iam_policy_document" "read_assume_policy" {
 resource "aws_iam_role" "read" {
   name               = "${var.cluster_name}-mango-read"
   description        = "This role allows read access to all relevant resources"
-  path               = "/ecs/"
   assume_role_policy = "${data.aws_iam_policy_document.read_trust_policy.json}"
 }
 
@@ -48,4 +47,9 @@ resource "aws_iam_policy" "read_assume_policy" {
 resource "aws_iam_role_policy_attachment" "read_cloudwatch_log_group" {
   role       = "${aws_iam_role.read.name}"
   policy_arn = "${aws_iam_policy.read_cloudwatch_log_group.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "read_codebuild" {
+  role       = "${aws_iam_role.read.name}"
+  policy_arn = "${aws_iam_policy.read_codebuild.arn}"
 }
