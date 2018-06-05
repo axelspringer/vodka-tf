91a9da0112e0a86dd34fb06a6c25279dfcf4f2d1
Ullrich <dullrich@XIN856904.asv.local>
2018-06-05 14:20:13 +0200

    added acm output for dns validation

--- acm/main.tf
+++ acm/main.tf
@@ -2,5 +2,5 @@
 resource "aws_acm_certificate" "cert" {
 	count = "${length(var.domain_list)}"
   domain_name = "${element(var.domain_list, count.index)}"
-  validation_method = "EMAIL"
+  validation_method = "DNS"
 }
--- acm/outputs.tf
+++ acm/outputs.tf
@@ -1,3 +1,16 @@
 output "certificate_arn" {
   value = "${aws_acm_certificate.cert.*.arn}"
 }
+
+output "dns_record" {
+  value = "${aws_acm_certificate.cert.*.resource_record_name}"
+}
+
+output "dns_type" {
+  value = "${aws_acm_certificate.cert.*.resource_record_type}"
+}
+
+output "dns_value" {
+  value = "${aws_acm_certificate.cert.*.resource_record_value}"
+}
+
